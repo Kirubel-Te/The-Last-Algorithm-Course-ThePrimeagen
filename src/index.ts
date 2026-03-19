afb3deaf8fd88fab286f987e8d1bdabd19d0053e
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2026-03-19 16:05:01 +0300

    two crystal ball problem solved

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { linearSearch } from "./LinerSearch";
 import binarySearch from "./BinarySearch";
 import BinarySearchIndex from "./BinarySearchIndex";
 import { mySqrt } from "./BinarySqrt";
+import twoCrystalBalls from "./Two_crystal_balls";
 
 // const arr = [1, 2, 3, 4, 5];
 // console.log(linearSearch(arr, 3));
@@ -13,8 +14,12 @@ import { mySqrt } from "./BinarySqrt";
     
 // console.log("Hello World");
 // console.log("this is the index.ts file...")
-console.log(mySqrt(4))
-console.log(mySqrt(8))
-console.log(mySqrt(1))
-console.log(mySqrt(0))
-console.log(mySqrt(57))
+// console.log(mySqrt(4))
+// console.log(mySqrt(8))
+// console.log(mySqrt(1))
+// console.log(mySqrt(0))
+// console.log(mySqrt(57))
+console.log(twoCrystalBalls([false, false, false, true, true]))
+console.log(twoCrystalBalls([false, false, false, false, false]))
+console.log(twoCrystalBalls([true, true, true, true, true]))
+console.log(twoCrystalBalls([false, true]))
